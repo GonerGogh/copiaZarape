8e3d29bffaee252635a5ef891abd41ca65590018
EmilioArzo <nemilion395@gmail.com>
2024-11-15 09:42:54 -0600

    Crear variables, constructor, setter y getter necesarios para clientes

--- zarape_Modelo/modeloClientes.java
+++ zarape_Modelo/modeloClientes.java
@@ -1,12 +1,103 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 /**
  *
  * @author Emilio
  */
 public class modeloClientes {
+    private int idCliente;
+    private int idPersona;
+    private int idusuario;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private int idCiudad;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    public modeloClientes(int idCliente, int idPersona, int idusuario, String nombre, String apellidos, String telefono, int idCiudad, String nombreUsuario, String contrasenia) {
+        this.idCliente = idCliente;
+        this.idPersona = idPersona;
+        this.idusuario = idusuario;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.idCiudad = idCiudad;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getIdCiudad() {
+        return idCiudad;
+    }
+
+    public void setIdCiudad(int idCiudad) {
+        this.idCiudad = idCiudad;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
     
 }
