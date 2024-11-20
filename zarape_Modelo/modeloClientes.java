ccf89d7613dbceea38ef4020f9568cf144d7d2aa
EmilioArzo <nemilion395@gmail.com>
2024-11-20 10:17:12 -0600

    Modificación para tener congruencia

--- zarape_Modelo/modeloClientes.java
+++ zarape_Modelo/modeloClientes.java
@@ -1,27 +1,20 @@
 
 public class modeloClientes {
+    public class Cliente {
     private int idCliente;
     private int idPersona;
-    private int idusuario;
-    private String nombre;
-    private String apellidos;
-    private String telefono;
-    private int idCiudad;
-    private String nombreUsuario;
-    private String contrasenia;
-
-    public modeloClientes(int idCliente, int idPersona, int idusuario, String nombre, String apellidos, String telefono, int idCiudad, String nombreUsuario, String contrasenia) {
+    private boolean activo;
+    private int idUsuario;
+    
+    
+    public Cliente(int idCliente, int idPersona, boolean activo, int idUsuario) {
         this.idCliente = idCliente;
         this.idPersona = idPersona;
-        this.idusuario = idusuario;
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.telefono = telefono;
-        this.idCiudad = idCiudad;
-        this.nombreUsuario = nombreUsuario;
-        this.contrasenia = contrasenia;
+        this.activo = activo;
+        this.idUsuario = idUsuario;
     }
-
+    
+    
     public int getIdCliente() {
         return idCliente;
     }
@@ -38,61 +31,21 @@ public class modeloClientes {
         this.idPersona = idPersona;
     }
 
-    public int getIdusuario() {
-        return idusuario;
+    public boolean isActivo() {
+        return activo;
     }
 
-    public void setIdusuario(int idusuario) {
-        this.idusuario = idusuario;
+    public void setActivo(boolean activo) {
+        this.activo = activo;
     }
 
-    public String getNombre() {
-        return nombre;
+    public int getIdUsuario() {
+        return idUsuario;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
     }
+}
 
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public int getIdCiudad() {
-        return idCiudad;
-    }
-
-    public void setIdCiudad(int idCiudad) {
-        this.idCiudad = idCiudad;
-    }
-
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
-
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
-
-    public String getContrasenia() {
-        return contrasenia;
-    }
-
-    public void setContrasenia(String contrasenia) {
-        this.contrasenia = contrasenia;
-    }
-    
-    
 }
