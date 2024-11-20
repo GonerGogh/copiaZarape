8f025c0c806a7bbe9c42eb9693282c8367dab5f0
EmilioArzo <nemilion395@gmail.com>
2024-11-20 10:18:04 -0600

    Creación del modulo de Usuario

--- zarape_Modelo/modeloUsuario.java
+++ zarape_Modelo/modeloUsuario.java
@@ -1,12 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Emilio
- */
 public class modeloUsuario {
+
+    private int idUsuario;
+    private String nombre;
+    private String contrasenia;
+    private int activo;
     
+    
+    public modeloUsuario(int idUsuario, String nombre, String contrasenia, int activo) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.contrasenia = contrasenia;
+        this.activo = activo;
+    }
+    
+    
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public void setActivo(int activo) {
+        this.activo = activo;
+    }
 }
